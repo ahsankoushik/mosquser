ffa2b4ae6a1c4e83e46d3c55b68a9c2a30b95089
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-06-23 14:34:17 +0600

    user .env removed

--- frontend/src/lib/api/client.svelte.ts
+++ frontend/src/lib/api/client.svelte.ts
@@ -57,8 +57,11 @@ class MosqClient{
 
 
     private constructor(){
-        this.host = import.meta.env.VITE_HOST;
-        // this.host = location.protocol + "//" + location.host
+        if(import.meta.env.MODE === "development"){
+            this.host = import.meta.env.VITE_HOST;
+        }else{
+            this.host = location.protocol + "//" + location.host
+        }
         this.headers = {
             "Content-Type" : "application/json", 
         };
